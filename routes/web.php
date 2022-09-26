c3ebc92e848e9f8b4a8f06a5ff062e3eea0fad60
SaBobic <fabrizioettori@gmail.com>
2022-09-26 15:14:55 +0200

    scaffolding and user migrations

--- routes/web.php
+++ routes/web.php
@@ -11,12 +11,12 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('guest.home');
 });
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/admin', 'HomeController@index')->name('home');
