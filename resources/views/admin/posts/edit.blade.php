c0c41dbefc98f3ce940ed8bc6d87438f57da8701
SaBobic <fabrizioettori@gmail.com>
2022-09-27 23:07:13 +0200

    merge create/edit form

--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -3,34 +3,7 @@
 @section('content')
 
 <div class="container">
-    <form action="{{ route('admin.posts.update', $post) }}" method="POST">
-        @csrf
-        @method('PUT')
-        <div class="form-row">
-            <div class="form-group col-md-12">
-                <label for="title">Title</label>
-                <input type="text" class="form-control" id="title" name="title" value="{{ old('title') ?? $post->title }}">
-            </div>
-        </div>
-
-        <div class="form-group">
-        <label for="image">Image URL</label>
-        <input type="url" class="form-control" id="image" name="image" value="{{ old('image') ?? $post->image }}">
-        </div>
-
-        <div class="form-group">
-            <label for="content">Content</label>
-            <textarea type="text" rows="5" class="form-control" id="content" name="content">{{ old('contect') ?? $post->content }}"</textarea>
-        </div>
-
-        <div class="form-group">
-            <div class="form-check">
-                <input class="form-check-input" type="checkbox" id="publish" name="publish">
-                <label class="form-check-label" for="publish">Publish</label>
-            </div>
-        </div>
-        <button type="submit" class="btn btn-success">Edit post</button>
-    </form>
+    @include('includes.posts.form')
 </div>
 
 @endsection
---
+++ resources/views/includes/posts/form.blade.php
@@ -0,0 +1,34 @@
+@if ($post->exists)
+<form action="{{ route('admin.posts.update', $post) }}" method="POST">
+    @method('PUT')
+@else
+<form action="{{ route('admin.posts.store') }}" method="POST">
+@endif
+
+
+    @csrf
+    <div class="form-row">
+        <div class="form-group col-md-12">
+            <label for="title">Title</label>
+            <input type="text" class="form-control" id="title" name="title" value="{{ old('title') ?? $post->title }}">
+        </div>
+    </div>
+
+    <div class="form-group">
+    <label for="image">Image URL</label>
+    <input type="url" class="form-control" id="image" name="image" value="{{ old('image') ?? $post->image }}">
+    </div>
+
+    <div class="form-group">
+        <label for="content">Content</label>
+        <textarea type="text" rows="5" class="form-control" id="content" name="content">{{ old('contect') ?? $post->content }}</textarea>
+    </div>
+
+    <div class="form-group">
+        <div class="form-check">
+            <input class="form-check-input" type="checkbox" id="publish" name="publish">
+            <label class="form-check-label" for="publish">Publish</label>
+        </div>
+    </div>
+    <button type="submit" class="btn btn-success">{{ $post->exists ? 'Edit post' : 'Create post' }}</button>
+</form>
