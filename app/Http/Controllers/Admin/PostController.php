401ad1f8867ef62e35bed1184f053ae89347e7a7
SaBobic <fabrizioettori@gmail.com>
2022-09-28 16:10:36 +0200

    added categories select into create/edit posts form

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
 use App\Models\Post;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
@@ -28,8 +29,9 @@ class PostController extends Controller
      */
     public function create()
     {
+        $categories = Category::all();
         $post = new Post;
-        return view('admin.posts.create', compact('post'));
+        return view('admin.posts.create', compact('post', 'categories'));
     }
 
     /**
@@ -78,7 +80,8 @@ class PostController extends Controller
      */
     public function edit(Post $post)
     {
-        return view('admin.posts.edit', compact('post'));
+        $categories = Category::all();
+        return view('admin.posts.edit', compact('post', 'categories'));
     }
 
     /**
--- resources/views/includes/posts/form.blade.php
+++ resources/views/includes/posts/form.blade.php
@@ -13,32 +13,41 @@
 <form action="{{ route('admin.posts.store') }}" method="POST" novalidate>
 @endif
 
-
     @csrf
 
-
     <div class="form-row">
-        <div class="form-group col-md-12">
-            <label for="title">Title</label>
+        <div class="form-group col-md-9">
+            <label for="title">Titolo</label>
             <input type="text" class="form-control" id="title" name="title" value="{{ old('title', $post->title) }}">
+        </div>
+        <div class="form-group col-md-3">
+            <label for="category">Categoria</label>
+            <select class="form-control" id="category" name="category_id">
+                <option>Scegli una categoria</option>
+                @foreach ($categories as $category)    
+                    <option
+                    @if (old('category_id', $post->category_id) == $category['id']) selected @endif
+                    value="{{ $category['id'] }}">{{ $category['label'] }}</option>
+                @endforeach
+            </select>
         </div>
     </div>
 
     <div class="form-group">
-    <label for="image">Image URL</label>
+    <label for="image">URL immagine</label>
     <input type="url" class="form-control" id="image" name="image" value="{{ old('image', $post->image) }}">
     </div>
 
     <div class="form-group">
-        <label for="content">Content</label>
+        <label for="content">Contenuto</label>
         <textarea type="text" rows="5" class="form-control" id="content" name="content">{{ old('content', $post->content) }}</textarea>
     </div>
 
     <div class="form-group">
         <div class="form-check">
             <input class="form-check-input" type="checkbox" id="publish" name="publish">
-            <label class="form-check-label" for="publish">Publish</label>
+            <label class="form-check-label" for="publish">Pubblica</label>
         </div>
     </div>
-    <button type="submit" class="btn btn-success">{{ $post->exists ? 'Edit post' : 'Create post' }}</button>
+    <button type="submit" class="btn btn-success">{{ $post->exists ? 'Modifica post' : 'Crea post' }}</button>
 </form>
